d870788379f1ea45723083ba6c39b4f2423ac0ee
manu12miskin <73878441+manu12miskin@users.noreply.github.com>
2021-01-31 18:41:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
-print("Hello world... ")
-print("Good Morning.. :-)")
+from flask import Flask 
+app = Flask(__name__) 
+   
+@app.route('/') 
+def hello_world(): 
+    return 'Hello World... '
+  
+@app.route('/morning') 
+def hello_world(): 
+    return 'Good morning... '
+  
+# main driver function 
+if __name__ == '__main__': 
+    app.run(host='0.0.0.0', port=8080)
