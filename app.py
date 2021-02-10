48c558b9d2299018fd6d13cf0b6a3a32a1623d98
manu12miskin <73878441+manu12miskin@users.noreply.github.com>
2021-02-11 00:35:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
    
 @app.route('/') 
 def hello_world(): 
-    return 'Hello World... '
+    return 'Hello World.... '
   
 @app.route('/morning') 
 def wish_me_good_morning(): 
-    return 'Good morning... '
+    return 'Good morning.... '
   
 # main driver function 
 if __name__ == '__main__': 
